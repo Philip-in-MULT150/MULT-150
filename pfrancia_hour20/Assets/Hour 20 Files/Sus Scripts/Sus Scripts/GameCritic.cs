873ca5bf734f2612930277c762c425cdafdc1628
Philip-in-MULT150 <pfrancia2002@outlook.com>
2025-05-22 09:57:10 -0700

    final

---
+++ pfrancia_hour20/Assets/Hour 20 Files/Sus Scripts/Sus Scripts/GameCritic.cs
@@ -0,0 +1,26 @@
+/*
+public class GameCritic : MonoBehaviour
+
+1.Is the game too easy or hard?
+Too easy because I was able to collect the Obstacle and Powerup. 
+Both the Obstacle and Powerup spawned more
+
+2.What 2 changes would you do to make it easier or harder?
+Have the Powerups appear less with mabye +3 seconds to ballance the game.
+Make the Obsticle ability more slow so the player would advoid it more.
+
+3.List 2 things that would give it a "wow" factor.
+The invisible ability that the player have.
+The extended time when the Powerup is picked up.
+
+4.What 2 parts of the game are fun?
+Controling the player ability.
+Avoiding Obsticles.
+
+5.What 2 parts of the game are tedious?
+Avoiding Obsticles.
+Timer counting down.
+
+
+
+*/
